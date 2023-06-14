a2da90ba0ea70dc035412e8a1144a8f57db5c2e0
Abdelrahman Elemam <74329630+Abdelrahmanelemam98@users.noreply.github.com>
2023-06-14 21:06:40 +0300

    update property,search,work us sections

--- real-state/src/app/share/home/home.module.ts
+++ real-state/src/app/share/home/home.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
-
-
+import { PropertyHomeComponent } from './components/property-home/property-home.component';
+import { SearchPropertyComponent } from './components/search-property/search-property.component';
+import { WorksSectionComponent } from './components/works-section/works-section.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [PropertyHomeComponent, SearchPropertyComponent, WorksSectionComponent],
+  imports: [CommonModule],
+  exports: [PropertyHomeComponent],
 })
-export class HomeModule { }
+export class HomeModule {}
